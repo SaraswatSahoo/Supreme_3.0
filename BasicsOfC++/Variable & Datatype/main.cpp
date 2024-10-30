8ec42b833d17eb994fd11706bde3f5b345d4f673
SaraswatSahoo <ssahoo.code.13@gmail.com>
2024-10-30 18:15:40 +0530

    commit 30/10/24

---
+++ BasicsOfC++/Variable & Datatype/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // //variable define
+    // int age = 19;
+    // cout << "My age is: " << age << endl;
+
+    //declaration
+    // int age;
+    // cout << age << endl;
+
+    //definition
+    // int age = 19;
+    // cout << age << endl;
+
+    //manipulation or updation
+    // age = 20;
+    // cout << age << endl;
+
+    //int 
+    // int count = 5;
+    // //float
+    // float share = 3.14;
+    // //char
+    // char alphabet = 'z';
+    // //double
+    // double weight = 55.69887;
+    // //boolean
+    // bool isMale = true;
+    // bool isChild = 1;
+    // bool isFemale = false;
+    // bool isGirl = 0;
+
+    // cout << count << endl;
+    // cout << share << endl;
+    // cout << alphabet << endl;
+    // cout << weight << endl;
+    // cout << isMale << endl;
+
+    int age = 12;
+    cout << sizeof(age) << endl;
+
+
+    return 0;
+}
